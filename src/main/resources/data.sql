78f50b57d047da29a1ce4ae7d35270bbdd3c3648
Rewax <daniel.lorenzen@hotmail.com>
2021-07-06 13:23:18 +0200

    backend

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,6 +15,7 @@
 --
 -- INSERT INTO about (description, img, title) VALUES ('desc', 'img', 'title');
 --
-INSERT INTO users (enabled, mail, password) VALUES (1, 'asd', '123');
+INSERT INTO users (enabled, mail, password) VALUES (1, 'daniel.lorenzen@hotmail.com', 'Predator88');
+INSERT INTO users (enabled, mail, password) VALUES (1, 'kim@tritræning.dk', 'Jern3xim');
 INSERT INTO auth (role, user_id) VALUES ('ROLE_ADMIN', 1)
 
